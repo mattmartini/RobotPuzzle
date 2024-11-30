399698e6da233143d8c2400d8e52057f84d9320c
Matt Martini <matt.martini@gmail.com>
2024-11-30 12:35:21 -0500

    feat(pytest): Configure pytest options; watch files and re-run tests

---
+++ run_pytest.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+source "$HOME/.bash/functions/utility_fns.sh"
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # yath stop
+}
+
+test_cmd=''
+
+make_test_cmd() {
+  local -A config
+  config=(  [h]='html' [html]=0 )
+  config+=( [c]='cov' [cov]=0 )
+  config+=( [p]='pretty' [pretty]=1 )
+  config+=( [r]='random' [random]=1 )
+  config+=( [v]='verbose' [verbose]=0 )
+
+  arg_parse "$@"
+
+  local cmd='python -m pytest '
+  local args=''
+
+  if [[ ${config[cov]} != 0 ]]; then
+    args+=' --cov=src --cov-branch '
+  fi
+
+  if [[ ${config[pretty]} != 1 ]]; then
+    args+=' -p no:sugar '
+  fi
+
+  if [[ ${config[random]} != 0 ]]; then
+    args+=' --random-order '
+  fi
+
+  if [[ ${config[verbose]} != 1 ]]; then
+    args+=' --no-header '
+  fi
+
+  if [[ ${config[html]} != 0 ]]; then
+    args+=' --html=tests/pytest_report.html ; open tests/pytest_report.html '
+  fi
+
+  test_cmd=$(printf "%s%s" "$cmd" "$args")
+}
+
+make_test_cmd "$@"
+#echo "$test_cmd"
+
+
+eval " fd . 'src/' 'tests/'  -e py | entr $test_cmd"
+
+
