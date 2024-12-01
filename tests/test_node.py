f93cfa1d272821c927659c6763b5466b3f7c3226
Matt Martini <matt.martini@gmail.com>
2024-11-30 23:17:33 -0500

    test(markers): Add markers for node and initial

--- tests/test_node.py
+++ tests/test_node.py
@@ -3,38 +3,50 @@
 import pytest
 from robotpuzzle.node import Node
 
-
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_repr(new_node):
     """Test initial repr"""
     cur_count = f"{Node.count - 1:03d}"
     assert repr(new_node) == f"Node({cur_count}, 0, None, {cur_count}, {cur_count})"
 
-
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_id(new_node):
     """Test initial id"""
     assert new_node.id == Node.count - 1
 
 
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_active(new_node):
     """Test action"""
     assert new_node.active == 0
 
 
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_data(new_node):
     """Test data"""
     assert new_node.data is None
 
 
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_prev(new_node):
     """Test pointer to prev node"""
     assert new_node.prev is new_node
 
 
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_next(new_node):
     """Test pointer to next node"""
     assert new_node.next is new_node
 
 
+@pytest.mark.initial
+@pytest.mark.node
 def test_initial_buffers(new_node):
     """Test data"""
     assert new_node.data is None
@@ -44,6 +56,7 @@ def test_initial_buffers(new_node):
     assert new_node.out_buffer_n is None
 
 
+@pytest.mark.node
 def test_activate(new_node):
     """Test that activate method activates node"""
     assert new_node.active == 0
@@ -51,6 +64,7 @@ def test_activate(new_node):
     assert new_node.active == 1
 
 
+@pytest.mark.node
 def test_deactivate(new_node):
     """Test that activate method activates node"""
     assert new_node.active == 0
@@ -60,6 +74,7 @@ def test_deactivate(new_node):
     assert new_node.active == 0
 
 
+@pytest.mark.node
 def test_turn_inside_out_and_explode(new_node):
     """Test that node explodes"""
     cur_count = f"{Node.count - 1:03d}"
