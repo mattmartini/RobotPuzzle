e6367755efb7b4acbcd8c39526954a2d21dbba68
Matt Martini <matt.martini@gmail.com>
2024-12-01 15:27:07 -0500

    refactor(node): Split tests: initial and methods

---
+++ tests/test_node_initial.py
@@ -0,0 +1,58 @@
+"""Tests for: Node for circular doubly linked list"""
+
+import pytest
+from robotpuzzle.node import Node
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_repr(new_node):
+    """Test initial repr"""
+    cur_count = f"{Node.count - 1:03d}"
+    assert repr(new_node) == f"Node({cur_count}, 0, None, {cur_count}, {cur_count})"
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_id(new_node):
+    """Test initial id"""
+    assert new_node.id == Node.count - 1
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_active(new_node):
+    """Test action"""
+    assert new_node.active == 0
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_data(new_node):
+    """Test data"""
+    assert new_node.data is None
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_prev(new_node):
+    """Test pointer to prev node"""
+    assert new_node.prev is new_node
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_next(new_node):
+    """Test pointer to next node"""
+    assert new_node.next is new_node
+
+
+@pytest.mark.initial
+@pytest.mark.node
+def test_initial_buffers(new_node):
+    """Test data"""
+    assert new_node.data is None
+    assert new_node.in_buffer_p is None
+    assert new_node.in_buffer_n is None
+    assert new_node.out_buffer_p is None
+    assert new_node.out_buffer_n is None
--- tests/test_node_methods.py
+++ tests/test_node_methods.py
@@ -4,60 +4,6 @@ import pytest
 from robotpuzzle.node import Node
 
 
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_repr(new_node):
-    """Test initial repr"""
-    cur_count = f"{Node.count - 1:03d}"
-    assert repr(new_node) == f"Node({cur_count}, 0, None, {cur_count}, {cur_count})"
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_id(new_node):
-    """Test initial id"""
-    assert new_node.id == Node.count - 1
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_active(new_node):
-    """Test action"""
-    assert new_node.active == 0
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_data(new_node):
-    """Test data"""
-    assert new_node.data is None
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_prev(new_node):
-    """Test pointer to prev node"""
-    assert new_node.prev is new_node
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_next(new_node):
-    """Test pointer to next node"""
-    assert new_node.next is new_node
-
-
-@pytest.mark.initial
-@pytest.mark.node
-def test_initial_buffers(new_node):
-    """Test data"""
-    assert new_node.data is None
-    assert new_node.in_buffer_p is None
-    assert new_node.in_buffer_n is None
-    assert new_node.out_buffer_p is None
-    assert new_node.out_buffer_n is None
-
-
 @pytest.mark.node
 def test_node_class_count_inc(new_node):
     """Test class Node counter is incremented after each new node"""
@@ -154,6 +100,7 @@ def test_take_action_tic(new_node):
     assert new_node.out_buffer_p is None
     assert new_node.out_buffer_n is None
 
+
 @pytest.mark.node
 def test_take_action_tock(new_node):
     """Test take action tock"""
@@ -171,5 +118,3 @@ def test_take_action_tock(new_node):
     new_node.in_buffer_p = 1
     new_node.in_buffer_n = 1
     returned = new_node.take_action("tock")
-
-
