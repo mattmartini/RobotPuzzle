b66639b0a79a06d5a13c025b6590f7e8d363f12a
Matt Martini <matt.martini@gmail.com>
2024-12-03 16:46:45 -0500

    test(fixtures): Add fixtures for CDLLs

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,12 @@
 
 import pytest
 from robotpuzzle.node import Node
+from robotpuzzle.robots import CDLL
 
 
 @pytest.fixture(name="multi_node")
 def fixture_new_node():
-    """Returns a new node instance"""
+    """Returns a new node instance function"""
 
     def _fixture_new_node():
         a_node = Node()
@@ -14,12 +15,31 @@ def fixture_new_node():
 
     return _fixture_new_node
 
+
 @pytest.fixture(name="new_node")
 def get_new_node():
     """Returns a new node instance"""
     return Node()
 
+
 @pytest.fixture(name="session_node")
 def fixture_session_node(scope="session"):
     """Returns a new session node instance"""
     return Node()
+
+
+@pytest.fixture(name="empty_circle")
+def get_empty_circle():
+    """Returns a new empty circle instance"""
+    return CDLL()
+
+
+@pytest.fixture(name="new_circle")
+def get_new_circle(num=0):
+    """Returns a new circle instance function"""
+
+    def _get_new_circle(num):
+        a_circle = CDLL(num)
+        return a_circle
+
+    return _get_new_circle
