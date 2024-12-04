f63ae62c991e850e43afe92d780f7303cca7d828
Matt Martini <matt.martini@gmail.com>
2024-12-04 13:06:33 -0500

    feat(docs): Update make_docs.sh to include changelog

--- make_docs.sh
+++ make_docs.sh
@@ -2,6 +2,15 @@
 
 set -Eeuo pipefail
 
+# Update API Docs
+echo -n "Updating API docs..."
 rm -rf docs/*
 fd -e py --exclude __init__.py --exclude bpython_setup.py -0 | xargs -0 pdoc  -o docs -d markdown --show-source --mermaid
+echo "done"
+
+# Update Changelog
+echo -n "Updating Changelog..."
+rm CHANGELOG.md
+git cliff > CHANGELOG.md
+echo "done"
 
