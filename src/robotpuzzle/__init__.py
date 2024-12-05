4b361840f5764f528e2880c5a730453a9d1d0914
Matt Martini <matt.martini@gmail.com>
2024-12-05 12:56:14 -0500

    feat(puzzle): Add commands for puzzle (uv run puzzle), bpython startup

--- src/robotpuzzle/__init__.py
+++ src/robotpuzzle/__init__.py
@@ -1,7 +1,28 @@
 """RobotPuzzle Init"""
-__date__ = '2024-12-04'
+
+__date__ = "2024-12-04"
 __version__ = "1.3.0"
+
+from rich import pretty, inspect, print
+from robotpuzzle import node
+from robotpuzzle import robots
+from robotpuzzle import buffer
+
 
 def main() -> None:
     print("Hello from robot puzzle!")
+    circle=robots.CDLL(2)
+    circle.create_robots()
+    inspect(circle)
+    circle.show_circle()
 
+    for seconds in range(2):
+        circle.run_clock()
+        circle.show_circle()
+
+    circle.head.activate()
+    circle.head.buffers.set_outputs(0,0)
+
+    for seconds in range(16):
+        circle.run_clock()
+        circle.show_circle()
