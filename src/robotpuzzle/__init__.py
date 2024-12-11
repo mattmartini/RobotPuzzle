154f219b497197e1495678fecc50a6449287d1bf
Matt Martini <matt.martini@gmail.com>
2024-12-10 19:47:59 -0500

    refactor(puzzle): Refactor puzzle main

--- src/robotpuzzle/__init__.py
+++ src/robotpuzzle/__init__.py
@@ -1,28 +1,39 @@
 """RobotPuzzle Init"""
 
-__date__ = '2024-12-05'
+__date__ = "2024-12-05"
 __version__ = "1.4.0"
 
 from rich import inspect
-from robotpuzzle import node
 from robotpuzzle import circles
-from robotpuzzle import buffer
+
+N = 2
+MAX_PRE_TIME = 2
+
+# The pre-time (before "Go" is pressed) should be a random number
+# For dev a value of 2 should suffice
 
 
 def main() -> None:
+    """Robot Puzzle Main"""
     print("Hello from robot puzzle!")
-    circle=circles.CDLL(2)
+    circle = circles.CDLL(N)
     circle.create_robots()
     inspect(circle)
     circle.show_circle()
 
-    for seconds in range(2):
+    # run the clock before "Go" is pressed (pre_time)
+    for _ in range(MAX_PRE_TIME):
         circle.run_clock()
         circle.show_circle()
 
+    # "Go" pressed.  Activates head.
+    # Should activate a random robot.
+    # (actually make a random robot head and activate it.)
     circle.head.activate()
-    circle.head.buffers.set_outputs(0,0)
+    circle.head.buffers.set_outputs(0, 0)
 
-    for seconds in range(6):
+    # run the clock
+    # should be a while true that runs until Boom!
+    for _ in range(6):
         circle.run_clock()
         circle.show_circle()
